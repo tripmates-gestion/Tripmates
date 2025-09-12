bf66e9e1931f5b31c334d9e1cf076bcef87f8abb
Leti <leticia.figueroa.r07@gmail.com>
2025-09-12 19:35:29 -0300

    chore: first commit. add initial structure for maven project with spring boot

---
+++ tripmates-backend/src/main/java/com/gestion/tripmates_backend/TripmatesBackendApplication.java
@@ -0,0 +1,13 @@
+package com.gestion.tripmates_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripmatesBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TripmatesBackendApplication.class, args);
+	}
+
+}
---
+++ tripmates-backend/src/test/java/com/gestion/tripmates_backend/TripmatesBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gestion.tripmates_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TripmatesBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
