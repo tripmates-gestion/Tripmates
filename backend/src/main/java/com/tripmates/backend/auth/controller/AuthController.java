b58c4e39cb1e2ce4aa70b8aec9b3a398dce9ab9c
FranInfanti <francisco.infanti@gmail.com>
2025-10-20 12:04:09 -0300

    feat(endpoint): Add logout and refresh endpoints

--- backend/src/main/java/com/tripmates/backend/auth/controller/AuthController.java
+++ backend/src/main/java/com/tripmates/backend/auth/controller/AuthController.java
@@ -1,7 +1,10 @@
 package com.tripmates.backend.auth.controller;
 
 import com.tripmates.backend.auth.dto.AuthLoginRequestDTO;
+import com.tripmates.backend.auth.dto.AuthLogoutRequestDTO;
+import com.tripmates.backend.auth.dto.AuthRefreshRequestDTO;
 import com.tripmates.backend.auth.service.AuthService;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 
 import org.springframework.http.ResponseEntity;
@@ -17,14 +20,38 @@ public class AuthController {
     public AuthController(AuthService authService) { this.authService = authService; }
 
     /**
-     * Endpoint para inicio de sesión.
+     * Endpoint para login de usuario
      *
-     * @param authLoginRequestDTO contiene email y password.
-     * @return access token y refresh token, {@link com.tripmates.backend.auth.dto.AuthLoginResponseDTO AuthLoginResponseDTO}.
+     * @param authLoginRequestDTO contiene email y password
+     * @return {@link com.tripmates.backend.auth.dto.AuthLoginResponseDTO AuthLoginResponseDTO}
      */
     @PostMapping("/login")
+    @ApiResponse(description = "Returns refresh and access token")
     public ResponseEntity<?> login(@RequestBody AuthLoginRequestDTO authLoginRequestDTO) {
-        return ResponseEntity.ok(this.authService.login(authLoginRequestDTO));
+        return ResponseEntity.ok(authService.login(authLoginRequestDTO));
     }
 
+    /**
+     * Endpoint para logout de usuario
+     *
+     * @param authLogoutRequestDTO contiene email
+     * @return {@link com.tripmates.backend.auth.dto.AuthLogoutResponseDTO AuthLogoutResponseDTO}
+     */
+    @PostMapping("/logout")
+    @ApiResponse(description = "Returns nothing")
+    public ResponseEntity<?> logout(@RequestBody AuthLogoutRequestDTO authLogoutRequestDTO) {
+        return ResponseEntity.ok(authService.logout(authLogoutRequestDTO));
+    }
+
+    /**
+     * Endpoint para refresh de access token
+     *
+     * @param authRefreshRequestDTO contiene email y refresh token
+     * @return {@link com.tripmates.backend.auth.dto.AuthRefreshResponseDTO AuthRefreshResponseDTO}
+     */
+    @PostMapping("/refresh")
+    @ApiResponse(description = "Returns access token")
+    public ResponseEntity<?> refresh(AuthRefreshRequestDTO authRefreshRequestDTO) {
+        return ResponseEntity.ok(authService.refresh(authRefreshRequestDTO));
+    }
 }
