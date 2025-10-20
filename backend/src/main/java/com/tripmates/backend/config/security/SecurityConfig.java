aadbc7abf24b0a2cccb556e0ec2e4e2f675879d9
FranInfanti <francisco.infanti@gmail.com>
2025-10-20 12:02:19 -0300

    feat(jwt): logout and refresh endpoints should be public

--- backend/src/main/java/com/tripmates/backend/config/security/SecurityConfig.java
+++ backend/src/main/java/com/tripmates/backend/config/security/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -15,12 +16,16 @@ import com.tripmates.backend.config.security.jwt.JwtAuthenticationFilter;
 @EnableWebSecurity
 public class SecurityConfig {
 
-        public static final String[] PUBLIC_ENDPOINTS = { "/users", "/auth/login", "/actuator/health",
-                        "/api-docs/**", // Ruta que configuraste en application.properties
-                        "/swagger-ui/**", // UI de Swagger
-                        "/swagger-ui.html", // HTML de Swagger UI
-                        "/swagger-resources/**", // Recursos de Swagger
-                        "/webjars/**" };
+        public static final String[] PUBLIC_ENDPOINTS = {
+                "/users",
+                "/auth/login", "/auth/refresh", "/auth/logout",
+                "/actuator/health",
+                "/api-docs/**", // Ruta que configuraste en application.properties
+                "/swagger-ui/**", // UI de Swagger
+                "/swagger-ui.html", // HTML de Swagger UI
+                "/swagger-resources/**", // Recursos de Swagger
+                "/webjars/**"
+        };
 
         @Autowired
         private JwtAuthenticationFilter authFilter;
@@ -28,7 +33,7 @@ public class SecurityConfig {
         @Bean
         public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
                 http
-                                .csrf(csrf -> csrf.disable())
+                                .csrf(AbstractHttpConfigurer::disable)
                                 .authorizeHttpRequests(auth -> auth
                                                 .requestMatchers(PUBLIC_ENDPOINTS).permitAll()
                                                 .anyRequest().authenticated())
