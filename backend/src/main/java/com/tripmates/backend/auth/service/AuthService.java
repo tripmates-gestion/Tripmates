9ba3aac394bae8fbd0b4e56105d7b77ee7b5c7e5
Leti <leticia.figueroa.r07@gmail.com>
2025-10-21 03:26:34 -0300

    fix(userService): move register from users controller to auth controller

--- backend/src/main/java/com/tripmates/backend/auth/service/AuthService.java
+++ backend/src/main/java/com/tripmates/backend/auth/service/AuthService.java
@@ -3,9 +3,12 @@ package com.tripmates.backend.auth.service;
 import com.tripmates.backend.auth.dto.*;
 import com.tripmates.backend.auth.exception.IncorrectPasswordException;
 import com.tripmates.backend.auth.exception.IncorrectTokenException;
+import com.tripmates.backend.auth.exception.UserAlreadyExistingException;
 import com.tripmates.backend.auth.exception.UserNotFoundException;
 import com.tripmates.backend.config.security.jwt.JwtService;
 import com.tripmates.backend.config.security.jwt.UserDetailFromJwt;
+import com.tripmates.backend.users.dto.UserCreationRequestDTO;
+import com.tripmates.backend.users.dto.UserCreationResponseDTO;
 import com.tripmates.backend.users.entity.mongo.User;
 import com.tripmates.backend.users.repository.mongo.UserRepository;
 
@@ -28,11 +31,42 @@ public class AuthService {
     private PasswordEncoder passwordEncoder;
 
     /**
+     * Crea un nuevo usuario y lo persiste en la base de datos MongoDB.
+     *
+     * @param userCreationRequestDTO dto con los datos del nuevo usuario.
+     * @return DTO con los tokens generados para el usuario.
+     */
+    public UserCreationResponseDTO createUser(UserCreationRequestDTO userCreationRequestDTO) {
+        var user = new User();
+        if (userRepository.findByEmail(userCreationRequestDTO.email()).isPresent()) {
+            throw new UserAlreadyExistingException("Email already in use");
+        }
+        user.setUsername(userCreationRequestDTO.username());
+        user.setEmail(userCreationRequestDTO.email());
+        user.setPassword(passwordEncoder.encode(userCreationRequestDTO.password()));
+        user.setRole(userCreationRequestDTO.role());
+        user.setDescription(userCreationRequestDTO.description());
+        user.setAvatarURL(userCreationRequestDTO.avatarURL());
+
+        var accessToken = jwtService.generateAccessToken(
+                new UserDetailFromJwt(user.getEmail(), user.getPassword()));
+
+        var refreshToken = jwtService.generateRefreshToken(
+                new UserDetailFromJwt(user.getEmail(), user.getPassword()));
+
+        user.setToken(refreshToken);
+        userRepository.save(user);
+
+        return new UserCreationResponseDTO(accessToken, refreshToken);
+    }
+
+    /**
      * Genera un access y refresh token para el usuario,
      * persiste en la base de datos el refresh token generado
      *
      * @param authLoginRequestDTO contiene email y password
-     * @return {@link com.tripmates.backend.auth.dto.AuthLoginResponseDTO AuthLoginResponseDTO}
+     * @return {@link com.tripmates.backend.auth.dto.AuthLoginResponseDTO
+     *         AuthLoginResponseDTO}
      */
     public AuthLoginResponseDTO login(AuthLoginRequestDTO authLoginRequestDTO) {
         User user = userRepository.findByEmail(authLoginRequestDTO.email())
@@ -43,12 +77,10 @@ public class AuthService {
         }
 
         var accessToken = this.jwtService.generateAccessToken(
-                new UserDetailFromJwt(user.getEmail(), user.getPassword())
-        );
+                new UserDetailFromJwt(user.getEmail(), user.getPassword()));
 
         var refreshToken = this.jwtService.generateRefreshToken(
-                new UserDetailFromJwt(user.getEmail(), user.getPassword())
-        );
+                new UserDetailFromJwt(user.getEmail(), user.getPassword()));
 
         user.setToken(refreshToken);
         userRepository.save(user);
@@ -61,7 +93,8 @@ public class AuthService {
      * del usuario
      *
      * @param authLogoutRequestDTO contiene email
-     * @return {@link com.tripmates.backend.auth.dto.AuthLogoutRequestDTO AuthLogoutRequestDTO}
+     * @return {@link com.tripmates.backend.auth.dto.AuthLogoutRequestDTO
+     *         AuthLogoutRequestDTO}
      */
     public AuthLogoutResponseDTO logout(AuthLogoutRequestDTO authLogoutRequestDTO) {
         User user = userRepository.findByEmail(authLogoutRequestDTO.email())
@@ -77,7 +110,8 @@ public class AuthService {
      * Retorna un nuevo access token para el usuario
      *
      * @param authRefreshRequestDTO contiene email y refresh token
-     * @return {@link com.tripmates.backend.auth.dto.AuthRefreshResponseDTO AuthRefreshResponseDTO}
+     * @return {@link com.tripmates.backend.auth.dto.AuthRefreshResponseDTO
+     *         AuthRefreshResponseDTO}
      */
     public AuthRefreshResponseDTO refresh(AuthRefreshRequestDTO authRefreshRequestDTO) {
         User user = userRepository.findByEmail(authRefreshRequestDTO.email())
@@ -88,8 +122,7 @@ public class AuthService {
         }
 
         var accessToken = this.jwtService.generateAccessToken(
-                new UserDetailFromJwt(user.getEmail(), user.getPassword())
-        );
+                new UserDetailFromJwt(user.getEmail(), user.getPassword()));
 
         return new AuthRefreshResponseDTO(accessToken);
     }
--- backend/src/main/java/com/tripmates/backend/users/service/UserService.java
+++ backend/src/main/java/com/tripmates/backend/users/service/UserService.java
@@ -1,20 +1,13 @@
 package com.tripmates.backend.users.service;
 
-import com.tripmates.backend.config.security.jwt.JwtService;
-import com.tripmates.backend.config.security.jwt.UserDetailFromJwt;
-import com.tripmates.backend.users.dto.UserCreationResponseDTO;
-import com.tripmates.backend.users.dto.UserCreationRequestDTO;
 import com.tripmates.backend.users.dto.UserProfileResponseDTO;
 import com.tripmates.backend.users.dto.DescriptionUpdateRequestDTO;
 import com.tripmates.backend.users.dto.UsernameUpdateRequestDTO;
 import com.tripmates.backend.users.entity.mongo.User;
 import com.tripmates.backend.users.repository.mongo.UserRepository;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
 import java.util.NoSuchElementException;
 
 @Service
@@ -23,42 +16,6 @@ public class UserService {
 
     @Autowired
     private UserRepository userRepository;
-
-    @Autowired
-    private JwtService jwtService;
-
-    @Autowired
-    private PasswordEncoder passwordEncoder;
-
-    /**
-     * Crea un nuevo usuario y lo persiste en la base de datos MongoDB.
-     *
-     * @param userCreationRequestDTO dto con los datos del nuevo usuario.
-     * @return DTO con los tokens generados para el usuario.
-     */
-    public UserCreationResponseDTO createUser(UserCreationRequestDTO userCreationRequestDTO) {
-        var user = new User();
-
-        user.setUsername(userCreationRequestDTO.username());
-        user.setEmail(userCreationRequestDTO.email());
-        user.setPassword(passwordEncoder.encode(userCreationRequestDTO.password()));
-        user.setRole(userCreationRequestDTO.role());
-        user.setDescription(userCreationRequestDTO.description());
-        user.setAvatarURL(userCreationRequestDTO.avatarURL()); // <-- Nuevo campo
-
-        var accessToken = jwtService.generateAccessToken(
-                new UserDetailFromJwt(user.getEmail(), user.getPassword())
-        );
-
-        var refreshToken = jwtService.generateRefreshToken(
-                new UserDetailFromJwt(user.getEmail(), user.getPassword())
-        );
-
-        user.setToken(refreshToken);
-        userRepository.save(user);
-
-        return new UserCreationResponseDTO(accessToken, refreshToken);
-    }
 
     /**
      * Obtiene el perfil de un usuario a partir de su email o nombre de usuario.
@@ -73,7 +30,7 @@ public class UserService {
 
         return new UserProfileResponseDTO(user);
     }
-    
+
     public UserProfileResponseDTO updateDescription(String emailOrUsername, DescriptionUpdateRequestDTO dto) {
         User user = userRepository.findByEmail(emailOrUsername)
                 .orElseThrow(() -> new NoSuchElementException("User not found"));
@@ -81,6 +38,7 @@ public class UserService {
         userRepository.save(user);
         return new UserProfileResponseDTO(user);
     }
+
     public UserProfileResponseDTO updateUsername(String emailOrUsername, UsernameUpdateRequestDTO dto) {
         User user = userRepository.findByEmail(emailOrUsername)
                 .orElseThrow(() -> new NoSuchElementException("User not found"));
