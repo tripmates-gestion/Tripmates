4eb6c9107d98b89d84a6de459d7f06af907185b9
wxmarcos <marcos@PC.localdomain>
2025-10-26 05:34:35 +0000

    Unifico endpoints de edicion de usuario

--- backend/src/main/java/com/tripmates/backend/users/controller/UserController.java
+++ backend/src/main/java/com/tripmates/backend/users/controller/UserController.java
@@ -13,8 +13,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
 import com.tripmates.backend.common.dto.ErrorDTO;
-import com.tripmates.backend.users.dto.UserUpdateDescriptionRequestDTO;
-import com.tripmates.backend.users.dto.UserUpdateUsernameRequestDTO;
+import com.tripmates.backend.users.dto.UserUpdateRequestDTO;
 import com.tripmates.backend.users.service.UserService;
 
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -50,35 +49,10 @@ public class UserController {
         return ResponseEntity.ok().body(userService.getUser(userDetails.getUsername()));
     }
   
-    @PatchMapping("/me/description")
-    @Operation(summary = "Updates profile description from a user in the system")
+    @PatchMapping("/me")
+    @Operation(summary = "Updates user profile information in the system")
     @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "Profile description updated successfully",
-                    content = { @Content(
-                            mediaType = "application/json",
-                            schema = @Schema(implementation = UserUpdateResponseDTO.class))
-                    }
-            ),
-            @ApiResponse(responseCode = "404", description = "User not found",
-                    content = { @Content(
-                            mediaType = "application/json",
-                            schema = @Schema(implementation = void.class))
-                    }
-            )
-    })
-    public ResponseEntity<?> updateDescription(
-            @AuthenticationPrincipal UserDetails userDetails,
-            @RequestBody UserUpdateDescriptionRequestDTO userUpdateDescriptionRequestDTO
-    ) {
-        return ResponseEntity.ok().body(
-                userService.updateDescription(userDetails.getUsername(), userUpdateDescriptionRequestDTO)
-        );
-    }
-
-    @PatchMapping("/me/username")
-    @Operation(summary = "Updates profile username from a user in the system")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "Profile username updated successfully",
+            @ApiResponse(responseCode = "200", description = "User profile updated successfully",
                     content = { @Content(
                             mediaType = "application/json",
                             schema = @Schema(implementation = UserUpdateResponseDTO.class))
@@ -91,12 +65,12 @@ public class UserController {
                     }
             )
     })
-    public ResponseEntity<?> updateUsername(
+    public ResponseEntity<?> updateProfile(
             @AuthenticationPrincipal UserDetails userDetails,
-            @RequestBody UserUpdateUsernameRequestDTO userUpdateDescriptionRequestDTO
+            @RequestBody UserUpdateRequestDTO userUpdateRequestDTO
     ) {
         return ResponseEntity.ok().body(
-                userService.updateUsername(userDetails.getUsername(), userUpdateDescriptionRequestDTO)
+                userService.updateUser(userDetails.getUsername(), userUpdateRequestDTO)
         );
     }
 }
