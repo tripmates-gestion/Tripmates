7963562ffc8180de747a3ccd036676788708681e
FranInfanti <francisco.infanti@gmail.com>
2025-10-28 01:44:58 -0300

    style(mongo): Added documentation for the user document attributest

--- backend/src/main/java/com/tripmates/backend/users/entity/mongo/User.java
+++ backend/src/main/java/com/tripmates/backend/users/entity/mongo/User.java
@@ -27,48 +27,106 @@ import java.time.LocalDate;
 @Document(collection = "users")
 public class User implements UserDetails {
 
+    /**
+     * User's ID.
+     */
     @Id
     private String id;
 
+    /**
+     * User's email.
+     */
     @NotNull
     @Indexed(unique = true)
     private String email;
 
+    /**
+     * User's username.
+     */
     @NotNull
     private String name;
 
+    /**
+     * User's password.
+     */
     @NotNull
     private String password;
 
-    private String description;
-    
+    /**
+     * User's business type.
+     * Only allowed in BUSINESS account.
+     */
     @NotNull
+    @Field(targetType = FieldType.STRING)
     private BusinessType businessType;
 
+    /**
+     * User's role.
+     */
     @NotNull
     @Field(targetType = FieldType.STRING)
     private Role role;
 
+    /**
+     * User's profile description.
+     */
+    @Field(targetType = FieldType.STRING)
+    private String description;
+
+    /**
+     * User's avatar profile URL.
+     */
+    @Field(targetType = FieldType.STRING)
     private String avatarURL;
 
+    /**
+     * User's refresh token.
+     */
+    @Field(targetType = FieldType.STRING)
     private String token;
 
+    /**
+     * User's business available days.
+     * Only allowed in BUSINESS account.
+     */
     private List<DayOfWeek> openingDays;
+
+    /**
+     * User's business attention schedule.
+     * Only allowed in BUSINESS account.
+     */
     private AttentionSchedule attentionSchedule;
+
+    /**
+     * User's business particular closing days.
+     * Only allowed in BUSINESS account.
+     */
     private List<LocalDate> exceptionalClosingDays;
+
+    /**
+     * User's phone number.
+     * Only allowed in BUSINESS account.
+     */
+    @Field(targetType = FieldType.STRING)
     private String phoneNumber;
+
+    /**
+     * User's business location.
+     * Only allowed in BUSINESS account.
+     */
+    @Field(targetType = FieldType.STRING)
     private String location;
+
+    /**
+     * User's profile pictures URLs.
+     */
     private List<String> profileImageUrls;
 
     @Override
-    public String getPassword() {
-        return this.password;
-    }
+    public String getPassword() { return this.password; }
 
     @Override
-    public String getUsername() {
-        return this.email; 
-    }
+    public String getUsername() { return this.email; }
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
@@ -76,22 +134,14 @@ public class User implements UserDetails {
     }
 
     @Override
-    public boolean isAccountNonExpired() {
-        return true;
-    }
+    public boolean isAccountNonExpired() { return true; }
 
     @Override
-    public boolean isAccountNonLocked() {
-        return true;
-    }
+    public boolean isAccountNonLocked() { return true; }
 
     @Override
-    public boolean isCredentialsNonExpired() { 
-        return true; 
-    }
+    public boolean isCredentialsNonExpired() { return true; }
 
     @Override
-    public boolean isEnabled() {
-        return true;
-    }
+    public boolean isEnabled() { return true; }
 }
