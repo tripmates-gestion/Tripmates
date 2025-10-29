5c446df6a924c690b1bad2d2b13c80b34f25b318
Leticia Figueroa <129856789+leticiafrR@users.noreply.github.com>
2025-10-29 09:10:36 -0300

    test(PublicationController): add basics  unit tests for publishment of publications

--- backend/src/test/java/com/tripmates/backend/publication/PublicationControllerTests.java
+++ backend/src/test/java/com/tripmates/backend/publication/PublicationControllerTests.java
@@ -5,38 +5,31 @@ import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.util.LinkedMultiValueMap;
-import org.springframework.util.MultiValueMap;
 
 import com.tripmates.backend.config.TestCloudinaryConfig;
-import com.tripmates.backend.config.TestSecurityConfig;
 
 import java.nio.charset.StandardCharsets;
-import java.time.DayOfWeek;
-import java.time.LocalDate;
-import java.time.LocalTime;
-import java.util.List;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.multipart;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.hamcrest.Matchers.*;
 
-import com.tripmates.backend.common.types.AttentionSchedule;
+import com.tripmates.backend.common.constants.ValidationErrorMessage;
 
-import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.TestInstance;
-
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.BeforeAll;
 import com.tripmates.backend.users.repository.mongo.UserRepository;
 
@@ -82,31 +75,143 @@ public class PublicationControllerTests {
 
 		String requestJson = """
 				{
-				    "description": "Beautiful place with amazing views and full amenities.",
-				    "phoneNumber": "+541112345678",
-				    "email": "contact@hostel.com",
-				    "location": "San Carlos de Bariloche, Argentina",
-				    "openingDays": ["MONDAY", "TUESDAY", "WEDNESDAY", "THURSDAY", "FRIDAY"],
-				    "attentionSchedule": {
-				        "openingTime": "09:00",
-				        "closingTime": "18:00"
-				    },
-				    "exceptionalClosingDays": ["2025-12-25", "2025-01-01"],
-				    "tags": ["hostel", "mountain", "nature"]
+				  "description": "Beautiful place with amazing views and full amenities.",
+				  "phoneNumber": "+541112345678",
+				  "email": "contact@hostel.com",
+				  "location": "San Carlos de Bariloche, Argentina",
+				  "openingDays": ["MONDAY", "TUESDAY", "WEDNESDAY", "THURSDAY", "FRIDAY"],
+				  "attentionSchedule": {
+				      "openingTime": "09:00",
+				      "closingTime": "18:00"
+				  },
+				  "exceptionalClosingDays": ["2025-12-25", "2025-01-01"],
+				  "tags": ["hostel", "mountain", "nature"]
 				}
 				""";
 
-		// 🔹 Construir la parte JSON como un “archivo” (sin nombre)
-		MockMultipartFile dataPart = new MockMultipartFile("data", // nombre del campo
-																	// esperado
-				"", // nombre del archivo (vacío)
-				"application/json", // content-type
+		MockMultipartFile dataPart = new MockMultipartFile("data", "", "application/json",
 				requestJson.getBytes(StandardCharsets.UTF_8));
 
-		// 🔹 Ejecutar la request multipart
 		mockMvc.perform(multipart("/publications/business").file(dataPart).header("Authorization", "Bearer " + jwt))
 			.andExpect(status().isBadRequest())
-			.andDo(print()); // imprime la request y response completas
+			.andExpect(jsonPath("$.type").value("about:blank"))
+			.andExpect(jsonPath("$.title").value("Validation Error"))
+			.andExpect(jsonPath("$.status").value(400))
+			.andExpect(jsonPath("$.detail").value("title: " + ValidationErrorMessage.EMPTY_OR_NULL_FIELD))
+			.andExpect(jsonPath("$.instance").value("/publications/business"))
+			.andDo(print());
+	}
+
+	@Test
+	void testGivenNoDescription_WhenCreatePublication_ThenShouldFailAndReturnError400() throws Exception {
+		String jwt = testHelper.getJwtTesting("test@example.com");
+
+		String requestJson = """
+				{
+				  "title": "Beautiful place with amazing views and full amenities.",
+				  "phoneNumber": "+541112345678",
+				  "email": "contact@hostel.com",
+				  "location": "San Carlos de Bariloche, Argentina",
+				  "openingDays": ["MONDAY", "TUESDAY", "WEDNESDAY", "THURSDAY", "FRIDAY"],
+				  "attentionSchedule": {
+				      "openingTime": "09:00",
+				      "closingTime": "18:00"
+				  },
+				  "exceptionalClosingDays": ["2025-12-25", "2025-01-01"],
+				  "tags": ["hostel", "mountain", "nature"]
+				}
+				""";
+
+		MockMultipartFile dataPart = new MockMultipartFile("data", "", "application/json",
+				requestJson.getBytes(StandardCharsets.UTF_8));
+
+		mockMvc.perform(multipart("/publications/business").file(dataPart).header("Authorization", "Bearer " + jwt))
+			.andExpect(status().isBadRequest())
+			.andExpect(jsonPath("$.type").value("about:blank"))
+			.andExpect(jsonPath("$.title").value("Validation Error"))
+			.andExpect(jsonPath("$.status").value(400))
+			.andExpect(jsonPath("$.detail").value("description: " + ValidationErrorMessage.EMPTY_OR_NULL_FIELD))
+			.andExpect(jsonPath("$.instance").value("/publications/business"))
+			.andDo(print());
+	}
+
+	@Test
+	void testGivenJustDescriptionAndTitle_WhenCreatePublication_ThenShouldSuccess() throws Exception {
+		String jwt = testHelper.getJwtTesting("test@example.com");
+
+		String requestJson = """
+				{
+				  "title": "Beautiful place with amazing views and full amenities.",
+				  "description": "Beautiful place with amazing views and full amenities."
+				}
+				""";
+
+		MockMultipartFile dataPart = new MockMultipartFile("data", "", "application/json",
+				requestJson.getBytes(StandardCharsets.UTF_8));
+		mockMvc.perform(multipart("/publications/business").file(dataPart).header("Authorization", "Bearer " + jwt))
+			.andExpect(status().isOk())
+			.andExpect(jsonPath("$.id").isNotEmpty())
+			.andExpect(jsonPath("$.title").value("Beautiful place with amazing views and full amenities."))
+			.andExpect(jsonPath("$.description").value("Beautiful place with amazing views and full amenities."))
+			.andExpect(jsonPath("$.openingDays").isArray())
+			.andExpect(jsonPath("$.openingDays").isEmpty())
+			.andExpect(jsonPath("$.attentionSchedule").value(Matchers.nullValue()))
+			.andExpect(jsonPath("$.phoneNumber").value(Matchers.nullValue()))
+			.andExpect(jsonPath("$.email").value(Matchers.nullValue()))
+			.andExpect(jsonPath("$.location").value(Matchers.nullValue()))
+			.andExpect(jsonPath("$.imageUrls").isArray())
+			.andExpect(jsonPath("$.imageUrls").isEmpty())
+			.andExpect(jsonPath("$.tags").isArray())
+			.andExpect(jsonPath("$.tags").isEmpty())
+			.andExpect(jsonPath("$.ownerId").isNotEmpty())
+			.andExpect(jsonPath("$.ownerUsername").exists())
+			.andExpect(jsonPath("$.createdAt").isNotEmpty())
+			.andDo(print());
+	}
+
+	@Test
+	void testGivenAllFieldsExceptImages_WhenCreatePublication_ThenShouldSuccess() throws Exception {
+		String jwt = testHelper.getJwtTesting("test@example.com");
+
+		String requestJson = """
+				{
+				  "title": "Beautiful place with amazing views and full amenities.",
+				  "description": "Beautiful place with amazing views and full amenities.",
+				  "phoneNumber": "+541112345678",
+				  "email": "contact@hostel.com",
+				  "location": "San Carlos de Bariloche, Argentina",
+				  "openingDays": ["MONDAY", "TUESDAY", "WEDNESDAY", "THURSDAY", "FRIDAY"],
+				  "attentionSchedule": {
+				      "openingTime": "09:00",
+				      "closingTime": "18:00"
+				  },
+				  "exceptionalClosingDays": ["2025-12-25", "2025-01-01"],
+				  "tags": ["hostel", "mountain", "nature"]
+				}
+				""";
+
+		MockMultipartFile dataPart = new MockMultipartFile("data", "", "application/json",
+				requestJson.getBytes(StandardCharsets.UTF_8));
+		mockMvc.perform(multipart("/publications/business").file(dataPart).header("Authorization", "Bearer " + jwt))
+			.andExpect(status().isOk())
+			.andExpect(jsonPath("$.id").isNotEmpty())
+			.andExpect(jsonPath("$.title").value("Beautiful place with amazing views and full amenities."))
+			.andExpect(jsonPath("$.description").value("Beautiful place with amazing views and full amenities."))
+			.andExpect(jsonPath("$.openingDays").isArray())
+			.andExpect(jsonPath("$.openingDays", contains("MONDAY", "TUESDAY", "WEDNESDAY", "THURSDAY", "FRIDAY")))
+			.andExpect(jsonPath("$.attentionSchedule.openingTime").value("09:00"))
+			.andExpect(jsonPath("$.attentionSchedule.closingTime").value("18:00"))
+			.andExpect(jsonPath("$.phoneNumber").value("+541112345678"))
+			.andExpect(jsonPath("$.email").value("contact@hostel.com"))
+			.andExpect(jsonPath("$.location").value("San Carlos de Bariloche, Argentina"))
+			.andExpect(jsonPath("$.imageUrls").isArray())
+			.andExpect(jsonPath("$.imageUrls").isEmpty())
+			.andExpect(jsonPath("$.tags").isArray())
+			.andExpect(jsonPath("$.tags", contains("hostel", "mountain", "nature")))
+			.andExpect(jsonPath("$.ownerId").isNotEmpty())
+			.andExpect(jsonPath("$.ownerUsername").exists())
+			.andExpect(jsonPath("$.createdAt").isNotEmpty())
+			.andDo(print());
 	}
 
 }
