d024158c8fd05e26f353d6d85dd92baa45870691
FranInfanti <francisco.infanti@gmail.com>
2025-10-29 20:57:16 -0300

    fix(test): Fix test

--- backend/src/test/java/com/tripmates/backend/publication/PublicationControllerTests.java
+++ backend/src/test/java/com/tripmates/backend/publication/PublicationControllerTests.java
@@ -5,6 +5,7 @@ import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.context.annotation.Import;
 import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.http.HttpHeaders;
 import org.springframework.test.context.ActiveProfiles;
 
 import com.tripmates.backend.config.TestCloudinaryConfig;
