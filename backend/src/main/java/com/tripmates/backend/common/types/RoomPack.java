856c95b3a1ed923bb6eee92555aa9ab54c572786
FranInfanti <francisco.infanti@gmail.com>
2025-11-01 13:50:31 -0300

    docs: Add documentation for records attributes

--- backend/src/main/java/com/tripmates/backend/common/types/RoomPack.java
+++ backend/src/main/java/com/tripmates/backend/common/types/RoomPack.java
@@ -3,16 +3,19 @@ package com.tripmates.backend.common.types;
 import java.time.LocalDate;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.v3.oas.annotations.media.Schema;
+
 import java.util.List;
 
 public record RoomPack(
-                @JsonFormat(pattern = "yyyy-MM-dd") LocalDate checkInDate,
-                @JsonFormat(pattern = "yyyy-MM-dd") LocalDate checkOutDate,
-                Integer numberOfGuests,
-                List<String> services,
-                Float price,
-                String description,
-                List<String> photosURLs
-
-) {
+		@Schema(description = "Room's check in date") @JsonFormat(
+				pattern = "yyyy-MM-dd") @JsonProperty("checkInDate") LocalDate checkInDate,
+		@Schema(description = "Room's check out day") @JsonFormat(
+				pattern = "yyyy-MM-dd") @JsonProperty("checkOutDate") LocalDate checkOutDate,
+		@Schema(description = "Room's number of guests") @JsonProperty("numberOfGuests") Integer numberOfGuests,
+		@Schema(description = "Room's services") @JsonProperty("services") List<String> services,
+		@Schema(description = "Room's price") @JsonProperty("price") Float price,
+		@Schema(description = "Room's description") @JsonProperty("description") String description,
+		@Schema(description = "Room's photos URLs") @JsonProperty("photosURLs") List<String> photosURLs) {
 }
