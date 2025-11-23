4e4cc6e5197674ea947c2656e410caab87ad1bd4
FranInfanti <francisco.infanti@gmail.com>
2025-11-23 01:52:23 -0300

    docs: Add documentation for history likes endpoint

--- backend/src/main/java/com/tripmates/backend/users/controller/UserController.java
+++ backend/src/main/java/com/tripmates/backend/users/controller/UserController.java
@@ -475,7 +475,8 @@ public class UserController {
 	}
 
 	@GetMapping("/history/likes/{userId}")
-	@Operation(summary = "Returns all the publications where the user has left a like")
+	@Operation(summary = "Returns all the publications where the user has left a like",
+    description = DocumentationObjectsExamples.USER_HISTORY_LIKES)
 	@ApiResponses(value = {
 			@ApiResponse(responseCode = "200", description = "Publications obtained successfully",
 					content = @Content(mediaType = "application/json",
