2773b1b242af75514c5521f52d421d81ba25c31a
FranInfanti <francisco.infanti@gmail.com>
2025-11-23 17:59:13 -0300

    test: Add new tests for coverage

--- backend/src/test/java/com/tripmates/backend/publication/PublicationTest.java
+++ backend/src/test/java/com/tripmates/backend/publication/PublicationTest.java
@@ -4,12 +4,15 @@ import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.tripmates.backend.TestHelper;
 import com.tripmates.backend.common.types.AttentionSchedule;
+import com.tripmates.backend.common.types.BusinessType;
 import com.tripmates.backend.config.TestCloudinaryConfig;
+import com.tripmates.backend.publications.dto.LikesListDTO;
 import com.tripmates.backend.publications.dto.PublicationResumeResponseDTO;
+import com.tripmates.backend.publications.dto.ReviewResponseDTO;
+import com.tripmates.backend.publications.dto.ReviewsListDTO;
 import com.tripmates.backend.publications.entity.mongo.Publication;
-import com.tripmates.backend.publications.repository.mongo.PublicationRepository;
 import com.tripmates.backend.users.dto.account.AccountResumeResponseDTO;
-import com.tripmates.backend.users.repository.mongo.AccountRepository;
+
 import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -21,7 +24,6 @@ import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.http.MediaType;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.mock.web.MockMultipartFile;
-import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -31,10 +33,7 @@ import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.List;
 
-import static org.hamcrest.Matchers.contains;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @AutoConfigureMockMvc
@@ -57,12 +56,6 @@ public class PublicationTest {
 	private ObjectMapper objectMapper;
 
 	@Autowired
-	private AccountRepository accountRepository;
-
-	@Autowired
-	private PublicationRepository publicationRepository;
-
-	@Autowired
 	private MockMvc mockMvc;
 
 	private TestHelper testHelper;
@@ -83,7 +76,7 @@ public class PublicationTest {
 		mongoTemplate.getDb().drop();
 	}
 
-	private PublicationResumeResponseDTO createPublication(String userTestingJwt) throws Exception {
+	private PublicationResumeResponseDTO createPublication(String businessAccountTestingJwt) throws Exception {
 		String requestJson = """
 				{
 				  "title": "Beautiful place with amazing views and full amenities.",
@@ -106,7 +99,7 @@ public class PublicationTest {
 
 		String body = mockMvc
 			.perform(multipart("/publications/business").file(dataPart)
-				.header("Authorization", "Bearer " + userTestingJwt))
+				.header("Authorization", "Bearer " + businessAccountTestingJwt))
 			.andExpect(status().isOk())
 			.andReturn()
 			.getResponse()
@@ -115,9 +108,27 @@ public class PublicationTest {
 		return objectMapper.readValue(body, PublicationResumeResponseDTO.class);
 	}
 
+	private ReviewResponseDTO createReview(String publicationId, String userAccountTestingJwt, String reviewJson)
+			throws Exception {
+		MockMultipartFile dataPart = new MockMultipartFile("data", "", "application/json",
+				reviewJson.getBytes(StandardCharsets.UTF_8));
+
+		String body = mockMvc
+			.perform(multipart("/publications/" + publicationId + "/review").file(dataPart).with(request -> {
+				request.setMethod("POST");
+				return request;
+			}).header("Authorization", "Bearer " + userAccountTestingJwt))
+			.andExpect(status().isCreated())
+			.andReturn()
+			.getResponse()
+			.getContentAsString();
+
+		return objectMapper.readValue(body, ReviewResponseDTO.class);
+	}
+
 	@Test
 	void testCanObtainPublicationsAfterBeingCreated() throws Exception {
-		String userTestingJwt = testHelper.getUserTestingJwt("contact@hostel.com");
+		String businessTestingJwt = testHelper.getBusinessTestingJwt("contact@hostel.com", BusinessType.HOTEL);
 
 		Publication publication = new Publication();
 		publication.setTitle("Beautiful place with amazing views and full amenities.");
@@ -134,18 +145,18 @@ public class PublicationTest {
 		List<PublicationResumeResponseDTO> expectedPublicationResumeResponseDTOList = List
 			.of(PublicationResumeResponseDTO.fromPublication(publication));
 
-		createPublication(userTestingJwt);
+		createPublication(businessTestingJwt);
 
 		String body = mockMvc
 			.perform(get("/publications/mine").accept(MediaType.APPLICATION_JSON)
-				.header("Authorization", "Bearer " + userTestingJwt))
+				.header("Authorization", "Bearer " + businessTestingJwt))
 			.andExpect(status().isOk())
 			.andReturn()
 			.getResponse()
 			.getContentAsString();
 
 		List<PublicationResumeResponseDTO> publicationResumeResponseDTOList = objectMapper.readValue(body,
-				new TypeReference<List<PublicationResumeResponseDTO>>() {
+				new TypeReference<>() {
 				});
 
 		Assertions.assertEquals(1, publicationResumeResponseDTOList.size());
@@ -177,26 +188,26 @@ public class PublicationTest {
 
 	@Test
 	void testCanDeleteAnExistingPublication() throws Exception {
-		String userTestingJwt = testHelper.getUserTestingJwt("contact@hostel.com");
+		String businessTestingJwt = testHelper.getBusinessTestingJwt("contact@hostel.com", BusinessType.HOTEL);
 
-		PublicationResumeResponseDTO publicationResumeResponseDTO = createPublication(userTestingJwt);
+		PublicationResumeResponseDTO publicationResumeResponseDTO = createPublication(businessTestingJwt);
 
 		mockMvc
 			.perform(delete("/publications/" + publicationResumeResponseDTO.id()).accept(MediaType.APPLICATION_JSON)
-				.header("Authorization", "Bearer " + userTestingJwt))
+				.header("Authorization", "Bearer " + businessTestingJwt))
 			.andExpect(status().isNoContent());
 
 		mockMvc
 			.perform(get("/publications/mine").accept(MediaType.APPLICATION_JSON)
-				.header("Authorization", "Bearer " + userTestingJwt))
+				.header("Authorization", "Bearer " + businessTestingJwt))
 			.andExpect(status().isOk());
 	}
 
 	@Test
 	void testCanUpdateAnExistingPublication() throws Exception {
-		String userTestingJwt = testHelper.getUserTestingJwt("contact@hostel.com");
+		String businessTestingJwt = testHelper.getBusinessTestingJwt("contact@hostel.com", BusinessType.HOTEL);
 
-		PublicationResumeResponseDTO publicationResumeResponseDTO = createPublication(userTestingJwt);
+		PublicationResumeResponseDTO publicationResumeResponseDTO = createPublication(businessTestingJwt);
 
 		String requestJson = """
 				{
@@ -211,7 +222,7 @@ public class PublicationTest {
 			.perform(multipart("/publications/" + publicationResumeResponseDTO.id()).file(dataPart).with(request -> {
 				request.setMethod("PATCH");
 				return request;
-			}).header("Authorization", "Bearer " + userTestingJwt))
+			}).header("Authorization", "Bearer " + businessTestingJwt))
 			.andExpect(status().isOk())
 			.andReturn()
 			.getResponse()
@@ -239,6 +250,138 @@ public class PublicationTest {
 						gotPublicationResumeResponseDTO.exceptionalClosingDays()),
 				() -> Assertions.assertEquals(publicationResumeResponseDTO.tags(),
 						gotPublicationResumeResponseDTO.tags()));
+	}
+
+	@Test
+	void testCanCreateAReviewFromAnExistingPublication() throws Exception {
+		String businessTestingJwt = testHelper.getBusinessTestingJwt("contact@hostel.com", BusinessType.HOTEL);
+		String userTestingJwt = testHelper.getUserTestingJwt("fran.infanti@gmail.com.ar");
+
+		PublicationResumeResponseDTO publicationResumeResponseDTO = createPublication(businessTestingJwt);
+
+		String requestJson = """
+				{
+				  "title": "Bla bla...",
+				  "content": "Bla bla bla bla...",
+				  "rating": 5.0
+				}
+				""";
+
+		ReviewResponseDTO reviewResponseDTO = createReview(publicationResumeResponseDTO.id(), userTestingJwt,
+				requestJson);
+
+		Assertions.assertAll(
+				() -> Assertions.assertEquals(publicationResumeResponseDTO, reviewResponseDTO.publicationReviewed()),
+				() -> Assertions.assertEquals("Bla bla...", reviewResponseDTO.title()),
+				() -> Assertions.assertEquals("Bla bla bla bla...", reviewResponseDTO.content()),
+				() -> Assertions.assertEquals(5.0, reviewResponseDTO.rating()),
+				() -> Assertions.assertEquals("fran.infanti@gmail.com.ar", reviewResponseDTO.reviewerUsername()));
+	}
+
+	@Test
+	void testMultipleUsersCanMadeAReviewOnAPublication() throws Exception {
+		String businessTestingJwt = testHelper.getBusinessTestingJwt("contact@hostel.com", BusinessType.HOTEL);
+
+		String franTestingJwt = testHelper.getUserTestingJwt("fran.infanti@gmail.com.ar");
+		String lewisTestingJwt = testHelper.getUserTestingJwt("lewis.hamilton44@gmail.com.gb");
+
+		PublicationResumeResponseDTO publicationResumeResponseDTO = createPublication(businessTestingJwt);
+
+		String franRequestJson = """
+				{
+				  "title": "Bla bla...",
+				  "content": "Bla bla bla bla...",
+				  "rating": 5.0
+				}
+				""";
+
+		String lewisRequestJson = """
+				{
+				  "title": "Also bla bla...",
+				  "content": "But do not forget, bla bla bla bla...",
+				  "rating": 4.5
+				}
+				""";
+
+		ReviewResponseDTO franReviewResponseDTO = createReview(publicationResumeResponseDTO.id(), franTestingJwt,
+				franRequestJson);
+		ReviewResponseDTO lewisReviewResponseDTO = createReview(publicationResumeResponseDTO.id(), lewisTestingJwt,
+				lewisRequestJson);
+
+		String body = mockMvc
+			.perform(get("/publications/" + publicationResumeResponseDTO.id() + "/review")
+				.accept(MediaType.APPLICATION_JSON)
+				.header("Authorization", "Bearer " + businessTestingJwt))
+			.andExpect(status().isOk())
+			.andReturn()
+			.getResponse()
+			.getContentAsString();
+
+		List<ReviewResponseDTO> reviewResponseDTOList = objectMapper.readValue(body, ReviewsListDTO.class).reviews();
+
+		Assertions.assertEquals(2, reviewResponseDTOList.size());
+		Assertions.assertEquals(List.of(franReviewResponseDTO, lewisReviewResponseDTO), reviewResponseDTOList);
+	}
+
+	@Test
+	void testCanLikeAnExistingPublication() throws Exception {
+		String businessTestingJwt = testHelper.getBusinessTestingJwt("contact@hostel.com", BusinessType.HOTEL);
+		String userTestingJwt = testHelper.getUserTestingJwt("fran.infanti@gmail.com.ar");
+
+		PublicationResumeResponseDTO publicationResumeResponseDTO = createPublication(businessTestingJwt);
+
+		mockMvc
+			.perform(post("/publications/" + publicationResumeResponseDTO.id() + "/like")
+				.accept(MediaType.APPLICATION_JSON)
+				.header("Authorization", "Bearer " + userTestingJwt))
+			.andExpect(status().isNoContent());
+
+		String body = mockMvc
+			.perform(get("/publications/" + publicationResumeResponseDTO.id() + "/likes")
+				.accept(MediaType.APPLICATION_JSON)
+				.header("Authorization", "Bearer " + userTestingJwt))
+			.andExpect(status().isOk())
+			.andReturn()
+			.getResponse()
+			.getContentAsString();
+
+		List<AccountResumeResponseDTO> likes = objectMapper.readValue(body, LikesListDTO.class).likes();
+
+		Assertions.assertEquals(1, likes.size());
+		Assertions.assertEquals("fran.infanti@gmail.com.ar", likes.getFirst().email());
+	}
+
+	@Test
+	void testCanLikeAndUnlikeAnExistingPublication() throws Exception {
+		String businessTestingJwt = testHelper.getBusinessTestingJwt("contact@hostel.com", BusinessType.HOTEL);
+		String userTestingJwt = testHelper.getUserTestingJwt("fran.infanti@gmail.com.ar");
+
+		PublicationResumeResponseDTO publicationResumeResponseDTO = createPublication(businessTestingJwt);
+
+		mockMvc
+			.perform(post("/publications/" + publicationResumeResponseDTO.id() + "/like")
+				.accept(MediaType.APPLICATION_JSON)
+				.header("Authorization", "Bearer " + userTestingJwt))
+			.andExpect(status().isNoContent());
+
+		mockMvc
+			.perform(post("/publications/" + publicationResumeResponseDTO.id() + "/unlike")
+				.accept(MediaType.APPLICATION_JSON)
+				.header("Authorization", "Bearer " + userTestingJwt))
+			.andExpect(status().isNoContent());
+
+		String body = mockMvc
+			.perform(get("/publications/" + publicationResumeResponseDTO.id() + "/likes")
+				.accept(MediaType.APPLICATION_JSON)
+				.header("Authorization", "Bearer " + userTestingJwt))
+			.andExpect(status().isOk())
+			.andReturn()
+			.getResponse()
+			.getContentAsString();
+
+		List<AccountResumeResponseDTO> likes = objectMapper.readValue(body, LikesListDTO.class).likes();
+
+		Assertions.assertEquals(0, likes.size());
 	}
 
 }
--- backend/src/test/java/com/tripmates/backend/users/AccountPlanTest.java
+++ backend/src/test/java/com/tripmates/backend/users/AccountPlanTest.java
@@ -1,6 +1,9 @@
 package com.tripmates.backend.users;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.tripmates.backend.common.types.Role;
+import com.tripmates.backend.community.dto.PlanWithPublicationsResponseDTO;
 import com.tripmates.backend.config.TestCloudinaryConfig;
 import com.tripmates.backend.publications.entity.mongo.Publication;
 import com.tripmates.backend.publications.repository.mongo.PublicationRepository;
@@ -19,6 +22,8 @@ import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.List;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
@@ -42,6 +47,9 @@ public class AccountPlanTest {
 
 	@Autowired
 	private MockMvc mockMvc;
+
+	@Autowired
+	private ObjectMapper objectMapper;
 
 	@BeforeEach
 	void beforeEach() {
@@ -85,6 +93,49 @@ public class AccountPlanTest {
 			.andExpect(jsonPath("$[0].description").value("Going fishing to Villa Paranacito next summer 2026"))
 			.andExpect(jsonPath("$[0].publications[0].title").value("Fishing Trip Publication"))
 			.andExpect(jsonPath("$[0].publications[0].description").value("Going now to Villa Paranacito"));
+	}
+
+	@Test
+	@WithMockUser(username = "franInfanti@gmail.com.ar", roles = { "USER" })
+	public void testDeletePlanAsUser() throws Exception {
+		Account fran = new Account();
+		fran.setName("Fran Infanti");
+		fran.setEmail("franInfanti@gmail.com.ar");
+		fran.setPassword("123456789");
+		fran.setRole(Role.USER);
+		accountRepository.save(fran);
+
+		Publication publication = new Publication();
+		publication.setTitle("Fishing Trip Publication");
+		publication.setDescription("Going now to Villa Paranacito");
+		publicationRepository.save(publication);
+
+		String createPlanRequest = String.format("""
+				{
+				  "name": "Fishing Trip 2026",
+				  "description": "Going fishing to Villa Paranacito next summer 2026",
+				  "publicationsIdList": ["%s"]
+				}
+				""", publication.getId());
+
+		mockMvc.perform(post("/users/plans/create").contentType(MediaType.APPLICATION_JSON).content(createPlanRequest))
+			.andExpect(status().isNoContent());
+
+		String body = mockMvc.perform(get("/community/list-plans").contentType(MediaType.APPLICATION_JSON))
+			.andExpect(status().isOk())
+			.andReturn()
+			.getResponse()
+			.getContentAsString();
+
+		List<PlanWithPublicationsResponseDTO> planWithPublicationsResponseDTOList = objectMapper.readValue(body,
+				new TypeReference<>() {
+				});
+
+		mockMvc
+			.perform(delete("/users/plans/" + planWithPublicationsResponseDTOList.getFirst().id())
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(createPlanRequest))
+			.andExpect(status().isNoContent());
 	}
 
 }
