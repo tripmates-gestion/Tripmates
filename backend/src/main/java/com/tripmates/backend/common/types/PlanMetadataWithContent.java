7509b7c869be04f32919f728ba96a93fb6958c74
Leticia Figueroa <129856789+leticiafrR@users.noreply.github.com>
2025-11-23 12:11:14 -0300

    style: apply format

--- backend/src/main/java/com/tripmates/backend/common/types/PlanMetadataWithContent.java
+++ backend/src/main/java/com/tripmates/backend/common/types/PlanMetadataWithContent.java
@@ -2,12 +2,6 @@ package com.tripmates.backend.common.types;
 
 import java.util.List;
 
-public record PlanMetadataWithContent(  
-  String planId,
-  String name, 
-  String description, 
-  String ownerId, 
-  List<String> collaboratorsIds, 
-  List<String> pendingUsersIdsInvited,
-  List<String> publicationsIds) {
+public record PlanMetadataWithContent(String planId, String name, String description, String ownerId,
+		List<String> collaboratorsIds, List<String> pendingUsersIdsInvited, List<String> publicationsIds) {
 }
