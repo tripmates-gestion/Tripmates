f284e1ca632fada0eb6009d6887442f328ac7910
FranInfanti <francisco.infanti@gmail.com>
2025-11-27 22:06:38 -0300

    feat: Add new field to ReviewCreationRequestDTO for mentions

--- backend/src/main/java/com/tripmates/backend/publications/dto/ReviewCreationRequestDTO.java
+++ backend/src/main/java/com/tripmates/backend/publications/dto/ReviewCreationRequestDTO.java
@@ -6,11 +6,14 @@ import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.Max;
 import com.tripmates.backend.common.constants.ValidationErrorMessage;
 
+import java.util.List;
+
 public record ReviewCreationRequestDTO(
 		@Schema(description = "Review title") @NotBlank(
 				message = ValidationErrorMessage.REVIEW_TITLE_BLANK) String title,
 		@Schema(description = "Review content") @NotBlank(
 				message = ValidationErrorMessage.REVIEW_CONTENT_BLANK) String content,
 		@Schema(description = "Review rating") @Min(value = 0, message = ValidationErrorMessage.REVIEW_RATING_MIN) @Max(
-				value = 5, message = ValidationErrorMessage.REVIEW_RATING_MAX) Double rating) {
+				value = 5, message = ValidationErrorMessage.REVIEW_RATING_MAX) Double rating,
+		@Schema(description = "Review's mentions") List<String> mentions) {
 }
