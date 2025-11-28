a991ddf0136ae854658db41fc291faa778a3ed47
FranInfanti <francisco.infanti@gmail.com>
2025-11-28 13:45:20 -0300

    feat: Add functionality to send notifications via email

--- backend/src/main/java/com/tripmates/backend/publications/dto/ReviewCreationRequestDTO.java
+++ backend/src/main/java/com/tripmates/backend/publications/dto/ReviewCreationRequestDTO.java
@@ -1,9 +1,14 @@
 package com.tripmates.backend.publications.dto;
 
+import com.fasterxml.jackson.annotation.JsonSetter;
+import com.fasterxml.jackson.annotation.Nulls;
+
 import io.swagger.v3.oas.annotations.media.Schema;
+
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.Max;
+
 import com.tripmates.backend.common.constants.ValidationErrorMessage;
 
 import java.util.List;
@@ -15,5 +20,5 @@ public record ReviewCreationRequestDTO(
 				message = ValidationErrorMessage.REVIEW_CONTENT_BLANK) String content,
 		@Schema(description = "Review rating") @Min(value = 0, message = ValidationErrorMessage.REVIEW_RATING_MIN) @Max(
 				value = 5, message = ValidationErrorMessage.REVIEW_RATING_MAX) Double rating,
-		@Schema(description = "Review's mentions") List<String> mentions) {
+		@Schema(description = "Review's mentions") @JsonSetter(nulls = Nulls.AS_EMPTY) List<String> mentions) {
 }
